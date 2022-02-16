20f6d2d14da110fbf325746844625b2c9eca5ae1
wamaithajoy <joyywamaitha@gmail.com>
2022-02-16 18:11:58 +0300

    First Record

---
+++ src/Student Records.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var name: String=("Leticia Wamaitha")
+    println(name)
+    var age=(23)
+    println(age)
+    var phonenumber: String=("0707161168")
+    println(phonenumber)
+    var weight_in_kg=64
+    println(weight_in_kg)
+    var Uganda: Boolean=true
+    println(Uganda)
+
+
+}
